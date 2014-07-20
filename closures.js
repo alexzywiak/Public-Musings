acd754b5ba7f60eff17cb121e097e068d9dfe153
Alex Zywiak <alex.zywiak@gmail.com>
2014-07-20 15:26:07 +0800

    Changed closures file to make use of Immediately Invoked Functions

--- closures.js
+++ closures.js
@@ -5,9 +5,13 @@ function idMaker( peopleList ){
   var len = peopleList.length;
   var uniqueId = 100;
   for( i = 0; i < len; i++){
-    peopleList[i].id = function() {
-      return uniqueId + i;
-    };
+    peopleList[i].id = function(j) {
+      debug('peopleList.id for ' + peopleList[j].name);
+      return function(){
+        debug('Inside Function for ' + peopleList[j].name);
+        return uniqueId + j;
+      }();
+    }(i);
   }
 
   return peopleList;
@@ -17,4 +21,4 @@ var list = [{name: 'Yossarian', id: 0}, {name: 'Nately', id: 0}, {name: 'Oar', i
 
 var idList = idMaker( list );
 
-debug( idList[0].id() );
+debug( idList[2].id );
--- simple-quiz.js
+++
@@ -1,6 +0,0 @@
-
-$(function($){
-
-  quiz.startQuiz();
-
-});
